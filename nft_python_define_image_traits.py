973cfe14178220203425deea4dc47816bbba8f5b
Jerry LIM <jerry@megapixel.sg>
2022-04-02 17:30:14 +0800

    Update nft_python_define_image_traits.py

--- nft_python_define_image_traits.py
+++ nft_python_define_image_traits.py
@@ -3,49 +3,44 @@
 # Each image is made up a series of traits
 # The weitages for each trait distribute the rarity and add up to 100%
 
-background = ["Silver", "Salmon", "Cantaloupe", "Mocha", "Teal"] 
-background_weights = [40, 30, 15, 10, 5]
+background = ["Blue", "Green", "Red", "Yellow"] 
+background_weights = [40, 30, 20, 10]
 
-triangle = ["Red", "Orange", "Yellow", "Green", "Blue"] 
-triangle_weights = [40, 30, 15, 10, 5]
+rice = ["White", "Blue", "Yellow"] 
+rice_weights = [50, 35, 15]
 
-circle = ["Red", "Orange", "Yellow", "Green", "Blue"] 
-circle_weights = [40, 30, 15, 10, 5]
+topping = ["Hiromi", "Roe", "Unagi", "Salmon"] 
+topping_weights = [40, 30, 20, 10]
 
-square = ["Red", "Orange", "Yellow", "Green", "Blue"] 
-square_weights = [40, 30, 15, 10, 5]
+face = ["DarkSad", "DarkHappy", "PinkSad", "PinkHappy"] 
+face_weights = [40, 30, 20, 10]
 
 # Dictionary variable for each trait. 
 # Each trait corresponds to its file name (from raw images from Github)
 
 background_files = {
-    "Silver": "background-silver",
-    "Salmon": "background-salmon",
-    "Cantaloupe": "background-cantaloupe",
-    "Teal": "background-teal",
-    "Mocha": "background-mocha",
+    "Blue": "background-blue",
+    "Green": "background-green",
+    "Red": "background-red",
+    "Yellow": "background-yellow",
 }
 
-triangle_files = {
-    "Red": "triangle-red",
-    "Orange": "triangle-orange",
-    "Yellow": "triangle-yellow",
-    "Green": "triangle-green",
-    "Blue": "triangle-blue",
+rice_files = {
+    "White": "rice-white",
+    "Blue": "rice-blue",
+    "Yellow": "rice-yellow",
 }
-circle_files = {
-    "Red": "circle-red",
-    "Orange": "circle-orange",
-    "Yellow": "circle-yellow",
-    "Green": "circle-green",
-    "Blue": "circle-blue",
+topping_files = {
+    "Hiromi": "topping-hiromi",
+    "Roe": "topping-roe",
+    "Unagi": "topping-unagi",
+    "Salmon": "topping-salmon",
 }
 
-square_files = {
-    "Red": "square-red",
-    "Orange": "square-orange",
-    "Yellow": "square-yellow",
-    "Green": "square-green",
-    "Blue": "square-blue",
+face_files = {
+    "DarkSad": "face-darksad",
+    "DarkHappy": "face-darkhappy",
+    "PinkSad": "face-pinksad",
+    "PinkHappy": "face-pinkhappy",
           
 }
