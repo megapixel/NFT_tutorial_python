06e4dc3e9a5aab1c9b66a966eaa9092ae58c308d
Jerry LIM <jerry@megapixel.sg>
2022-04-02 17:31:38 +0800

    Create opt_nft_python_check_unique.py

---
+++ opt_nft_python_check_unique.py
@@ -0,0 +1,8 @@
+##OPTIONAL 
+
+# Returns true if all images are unique
+def all_images_unique(all_images):
+    seen = list()
+    return not any(i in seen or seen.append(i) for i in all_images)
+
+print("Are all images unique?", all_images_unique(all_images))
