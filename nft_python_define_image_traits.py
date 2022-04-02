57d182d4fb56f20cc53d7b379dab22c3c9d7775e
Jerry LIM <jerry@megapixel.sg>
2022-04-02 09:15:19 +0800

    Create nft_python_define_image_traits.py

---
+++ nft_python_define_image_traits.py
@@ -0,0 +1,51 @@
+## Main Snippet 2
+
+# Each image is made up a series of traits
+# The weitages for each trait distribute the rarity and add up to 100%
+
+background = ["Silver", "Salmon", "Cantaloupe", "Mocha", "Teal"] 
+background_weights = [40, 30, 15, 10, 5]
+
+triangle = ["Red", "Orange", "Yellow", "Green", "Blue"] 
+triangle_weights = [40, 30, 15, 10, 5]
+
+circle = ["Red", "Orange", "Yellow", "Green", "Blue"] 
+circle_weights = [40, 30, 15, 10, 5]
+
+square = ["Red", "Orange", "Yellow", "Green", "Blue"] 
+square_weights = [40, 30, 15, 10, 5]
+
+# Dictionary variable for each trait. 
+# Each trait corresponds to its file name (from raw images from Github)
+
+background_files = {
+    "Silver": "background-silver",
+    "Salmon": "background-salmon",
+    "Cantaloupe": "background-cantaloupe",
+    "Teal": "background-teal",
+    "Mocha": "background-mocha",
+}
+
+triangle_files = {
+    "Red": "triangle-red",
+    "Orange": "triangle-orange",
+    "Yellow": "triangle-yellow",
+    "Green": "triangle-green",
+    "Blue": "triangle-blue",
+}
+circle_files = {
+    "Red": "circle-red",
+    "Orange": "circle-orange",
+    "Yellow": "circle-yellow",
+    "Green": "circle-green",
+    "Blue": "circle-blue",
+}
+
+square_files = {
+    "Red": "square-red",
+    "Orange": "square-orange",
+    "Yellow": "square-yellow",
+    "Green": "square-green",
+    "Blue": "square-blue",
+          
+}
